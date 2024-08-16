298c34e1e14037695bd3088e8edfb4f5ef2c0b9c
Vlad-student <vlad.v.terletskiy@gmail.com>
2024-08-16 19:06:46 +0300

    dfdf

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const answers = document.querySelectorAll('#faq .faq-ask');
 questions.forEach((question) => {
     console.dir(question)
     question.addEventListener('click', ()=>{
-question.nextElementSibling.classList.add('open')
+// question.nextElementSibling.classList.add('open')
+const answerCurrent = question.nextElementSibling;
+const plus = question.lastChild;
+
+answers.forEach((answer) => {
+    answer.classList.remove('open');
+    answer.computedStyleMap.maxHeight = 0+'px'
+});
+
+plus.textContent ='-'
+answerCurrent.classList.add('open')
+answerCurrent.style.maxHeight = answerCurrent.scrollHeight+'px'
     })
 });
+
